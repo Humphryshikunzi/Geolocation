6f7e1407fc6d8f55ece04cfe360286e7185c7abc
Humphryshikunzi <humphryshikunzi9@gmail.com>
2019-02-21 08:43:46 +0300

    Added Geolocation

---
+++ Geolocation/LocationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Geolocation
+{
+  public  class LocationManager
+    {
+        public async static Task<Geoposition> GetGeoPosition()
+        {
+            var accessstatus =await Geolocator.RequestAccessAsync();
+            if(accessstatus!=GeolocationAccessStatus.Allowed) throw new Exception();
+
+            var geolocator=new Geolocator{DesiredAccuracyInMeters = 0};
+            var postion = await geolocator.GetGeopositionAsync();
+            return postion;
+            
+        }
+    }
+}
